723847ebcc79f116c8f19beb411a098b2425d909
CSardo01 <sardc042@newschool.edu>
2023-02-26 02:28:09 -0500

    launch

---
+++ hover-scripts.js
@@ -0,0 +1,32 @@
+let button3 = document.querySelector("#b3");
+let img3 = document.querySelector("#i3");
+button3.onmouseenter = () => {
+    img3.classList.toggle("show")
+};
+button3.onmouseleave= () => {
+    img3.classList.toggle("show")
+};
+
+
+let button2 = document.querySelector("#b2");
+let img2 = document.querySelector("#i2");
+button2.onmouseenter = () => {
+    img2.classList.toggle("show")
+};
+button2.onmouseleave= () => {
+    img2.classList.toggle("show")
+};
+
+let button1 = document.querySelector("#b1");
+let img1 = document.querySelector("#i1");
+button1.onmouseenter = () => {
+    img1.classList.toggle("show")
+};
+button1.onmouseleave= () => {
+    img1.classList.toggle("show")
+};
+
+
+
+
+
