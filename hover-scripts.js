afa059442b987d31078079319e858979bf341633
CSardo01 <sardc042@newschool.edu>
2023-02-27 00:10:33 -0500

    uhh

--- hover-scripts.js
+++ hover-scripts.js
@@ -1,10 +1,15 @@
+let logo = document.querySelector(".logo");
+
+
 let button3 = document.querySelector("#b3");
 let img3 = document.querySelector("#i3");
 button3.onmouseenter = () => {
     img3.classList.toggle("show")
+    logo.classList.toggle("gone")
 };
 button3.onmouseleave= () => {
     img3.classList.toggle("show")
+    logo.classList.toggle("gone")
 };
 
 
@@ -12,18 +17,22 @@ let button2 = document.querySelector("#b2");
 let img2 = document.querySelector("#i2");
 button2.onmouseenter = () => {
     img2.classList.toggle("show")
+    logo.classList.toggle("gone")
 };
 button2.onmouseleave= () => {
     img2.classList.toggle("show")
+    logo.classList.toggle("gone")
 };
 
 let button1 = document.querySelector("#b1");
 let img1 = document.querySelector("#i1");
 button1.onmouseenter = () => {
     img1.classList.toggle("show")
+    logo.classList.toggle("gone")
 };
 button1.onmouseleave= () => {
     img1.classList.toggle("show")
+    logo.classList.toggle("gone")
 };
 
 
